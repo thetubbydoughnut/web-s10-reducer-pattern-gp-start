02631ba4508e15044cf5146c7a64d115aebe08ed
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-06-24 14:17:47 -0500

    Finished the GP

--- frontend/components/TodoForm.js
+++ frontend/components/TodoForm.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
-export default function TodoForm() {
+const CHANGE_LABEL = 'CHANGE_LABEL'
+const CHANGE_IS_COMPLETED = 'CHANGE_IS_COMPLETED'
+
+const initialState = {
+  todoLabel: '',
+  todoIsCompleted: false,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CHANGE_LABEL: 
+      return {...state, todoLabel: action.payload}
+    case CHANGE_IS_COMPLETED:
+      return {...state, todoIsCompleted: action.payload}
+    default:
+      return state
+
+  }
+}
+
+export default function TodoForm({ createNewTodo }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const onLabelChange = ({ target: { value } }) => {
+    dispatch({ type: CHANGE_LABEL, payload: value})
+   }
+  const onIsCompletedChange = ({ target: { checked } }) => {
+    dispatch({ type: CHANGE_IS_COMPLETED, payload: checked})
+   }
+  const resetForm = () => { 
+    dispatch({ type: CHANGE_LABEL, payload: ''})
+    dispatch({ type: CHANGE_IS_COMPLETED, payload: false})
+  }
+  
+  const onNewTodo = evt => {
+    evt.preventDefault()
+    createNewTodo(state.todoLabel, state.todoIsCompleted)
+    resetForm()
+  }
+
   return (
-    <form id="todoForm">
+    <form id="todoForm" onSubmit={onNewTodo}>
       <h3>New Todo Form</h3>
       <label><span>Todo label:</span>
         <input
+          onChange={onLabelChange}
+          value={state.todoLabel}
           type='text'
           name='todoLabel'
           placeholder='Type label'
@@ -13,6 +53,8 @@ export default function TodoForm() {
       </label>
       <label><span>Is completed:</span>
         <input
+          onChange={onIsCompletedChange}
+          checked={state.todoIsCompleted}
           type='checkbox'
           name='todoIsCompleted'
         />
--- frontend/components/Todos.js
+++ frontend/components/Todos.js
@@ -8,6 +8,9 @@ const StyledTodo = styled.li`
 
 export default function Todo({
   todos,
+  toggleTodo,
+  showCompletedTodos,
+  toggleShowCompletedTodos
 }) {
   return (
     <div id="todos">
@@ -15,15 +18,20 @@ export default function Todo({
       <ul>
         {
           todos
+            ?.filter(todo => {
+              return showCompletedTodos || !todo.complete
+            })
             .map(todo => (
-              <StyledTodo $complete={todo.complete} key={todo.id}>
+              <StyledTodo 
+              onClick={() => toggleTodo(todo.id)} 
+              $complete={todo.complete} key={todo.id}>
                 <span>{todo.label}{todo.complete && ' ✔️'}</span>
               </StyledTodo>
             ))
         }
       </ul>
-      <button>
-        Hide completed todos
+      <button onClick={toggleShowCompletedTodos}>
+        {showCompletedTodos ? 'Hide' : 'Show'} completed todos
       </button>
     </div>
   )
